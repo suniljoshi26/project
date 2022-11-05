a0a9d41e54d31e8b2969fc5debe4bdefea015901
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-05 10:06:42 +0530

    rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home.jsx";
+
+import Login from "./Login.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="bg-red-500">hello</div>;
+  return (
+    <div className="bg-red-500">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route index element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
